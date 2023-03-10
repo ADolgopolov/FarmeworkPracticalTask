b2dda3cbaa4a20f7387498c289866f5893a203a4
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2023-03-10 13:17:43 +0200

    change qa.propertes

--- framework/src/test/java/com/epam/training/student_andrii_dolhopolov/test/AbstractTest.java
+++ framework/src/test/java/com/epam/training/student_andrii_dolhopolov/test/AbstractTest.java
@@ -1,10 +1,15 @@
 package com.epam.training.student_andrii_dolhopolov.test;
 
 import com.epam.training.student_andrii_dolhopolov.driver.DriverSingleton;
+import com.epam.training.student_andrii_dolhopolov.util.TestListener;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Log4j2
+@Listeners({TestListener.class})
 public class AbstractTest {
     protected WebDriver driver;
     protected String searchTerm = "Google Cloud Platform Pricing Calculator";
